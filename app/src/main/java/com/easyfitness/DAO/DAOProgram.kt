9ff380d9d433eaf0109f5a7bd57230a5f1c3ad07
Michał Urbanek <urbanekmike@gmail.com>
2025-08-06 10:01:33 +0200

    compose version still not remember how to turn it

--- app/src/main/java/com/easyfitness/DAO/DAOProgram.kt
+++ app/src/main/java/com/easyfitness/DAO/DAOProgram.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import java.security.AccessControlContext
+import java.util.Collections
 
 class DAOProgram(context: Context?) : DAOBase(context) {
     private var mCursor: Cursor? = null
@@ -69,14 +70,14 @@ class DAOProgram(context: Context?) : DAOBase(context) {
         return value
     }
 
-    val allProgramsNames: MutableList<String>?
+    val allProgramsNames: MutableList<String>
         get() {
             val programs: MutableList<String> = ArrayList()
             val db = this.readableDatabase
             mCursor = null
             mCursor = db.query(TABLE_NAME, arrayOf(PROGRAM_NAME), null, null, null, null, PROGRAM_NAME)
             if (mCursor != null) mCursor!!.moveToFirst()
-            if (mCursor!!.count == 0) return null
+            if (mCursor!!.count == 0) return emptyList<String>() as MutableList<String>
             if (mCursor!!.moveToFirst()) {
                 do {
                     programs.add(mCursor!!.getString(mCursor!!.getColumnIndex(PROGRAM_NAME)))
@@ -94,6 +95,25 @@ class DAOProgram(context: Context?) : DAOBase(context) {
             return getProgramListCursor(selectQuery)
         }
 
+    val allProgramsToView: MutableList<Program>
+        get() {
+            val programs: MutableList<Program> = ArrayList()
+            val db = this.readableDatabase
+            mCursor = null
+            mCursor = db.query(TABLE_NAME, arrayOf(PROGRAM_NAME,KEY), null, null, null, null, PROGRAM_NAME)
+            if (mCursor != null) mCursor!!.moveToFirst()
+            if (mCursor!!.count == 0) return Collections.emptyList()
+            if (mCursor!!.moveToFirst()) {
+                do {
+                    val program = Program(mCursor!!.getString(mCursor!!.getColumnIndex(PROGRAM_NAME)), 1)
+                    program.id=mCursor!!.getLong(mCursor!!.getColumnIndex(KEY))
+                    programs.add(program)
+                } while (mCursor!!.moveToNext())
+            }
+            mCursor!!.close()
+            close()
+            return programs
+        }
     /**
      * @return List of Machine object ordered by Favorite and Name
      */
--- app/src/main/java/com/easyfitness/fonte/FontesPagerFragment.kt
+++ app/src/main/java/com/easyfitness/fonte/FontesPagerFragment.kt
@@ -1,98 +1,87 @@
-package com.easyfitness.fonte;
+package com.easyfitness.fonte
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import com.easyfitness.R
+import com.easyfitness.programs.NonSwipeableViewPager
+import com.easyfitness.programs.ProgramRunner
+import com.easyfitness.programs.ProgramRunnerComposeViewModel
+import com.ogaclejapan.smarttablayout.SmartTabLayout
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
 
-import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager.widget.ViewPager.OnPageChangeListener;
+class FontesPagerFragment : Fragment() {
+    private var pagerAdapter: FragmentPagerItemAdapter? = null
 
-import com.easyfitness.R;
-import com.easyfitness.programs.NonSwipeableViewPager;
-import com.easyfitness.programs.ProgramRunner;
-import com.ogaclejapan.smarttablayout.SmartTabLayout;
-import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
-import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
-
-import java.util.Objects;
-
-public class FontesPagerFragment extends Fragment {
-    private FragmentPagerItemAdapter pagerAdapter = null;
-    //    private String name;
-//    private int id;
-//    private FontesFragment mpFontesFrag = null;
-//    private FonteHistoryFragment mpHistoryFrag = null;
-//    private FonteGraphFragment mpGraphFrag = null;
-
-    /**
-     * Create a new instance of DetailsFragment, initialized to
-     * show the text at 'index'.
-     */
-    public static FontesPagerFragment newInstance(String name, int id) {
-        FontesPagerFragment f = new FontesPagerFragment();
-
-        // Supply index input as an argument.
-        Bundle args = new Bundle();
-        args.putString("name", name);
-        args.putInt("id", id);
-        f.setArguments(args);
-
-        return f;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.programrunner_pager, container, false);
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.programrunner_pager, container, false)
 
         // Locate the viewpager in activity_main.xml
-        NonSwipeableViewPager mViewPager = view.findViewById(R.id.programrunner_pager);
+        val mViewPager = view.findViewById<NonSwipeableViewPager>(R.id.programrunner_pager)
 
         if (mViewPager.getAdapter() == null) {
+            val args = this.getArguments()
+            args!!.putLong("machineID", -1)
+            args.putLong("machineProfile", -1)
 
-            Bundle args = this.getArguments();
-            args.putLong("machineID", -1);
-            args.putLong("machineProfile", -1);
 
-            pagerAdapter = new FragmentPagerItemAdapter(
-                getChildFragmentManager(), FragmentPagerItems.with(this.getContext())
-                .add(R.string.ProgramRunnerLabel, ProgramRunner.class)
-//                .add(R.string.ExerciceLabel, FontesFragment.class)
-//                .add(R.string.GraphLabel, FonteGraphFragment.class, args)
-//                .add(R.string.HistoryLabel, FonteHistoryFragment.class, args)
-                .create());
+//            pagerAdapter =
+//                FragmentPagerItemAdapter( //                getChildFragmentManager(), FragmentPagerItems.with(this.getContext())
+//                    //                .add(R.string.ProgramRunnerLabel, ProgramRunnerScreenKt.class)
+//                    getChildFragmentManager(), FragmentPagerItems.with(this.getContext())
+//                        .add(
+//                            R.string.ProgramRunnerLabel,
+//                            ProgramRunnerComposeViewModel::class.java
+//                        )
+////                        FragmentPagerItemAdapter( //                getChildFragmentManager(), FragmentPagerItems.with(this.getContext())
+////                    //                .add(R.string.ProgramRunnerLabel, ProgramRunnerScreenKt.class)
+////                    getChildFragmentManager(), FragmentPagerItems.with(this.getContext())
+////                        .add(
+////                            R.string.ProgramRunnerLabel,
+////                            ProgramRunner::class.java
+////                        )
+//                        //                .add(R.string.ExerciceLabel, FontesFragment.class)
+//                        //                .add(R.string.GraphLabel, FonteGraphFragment.class, args)
+//                        //                .add(R.string.HistoryLabel, FonteHistoryFragment.class, args)
+//                        .create()
+//                )
 
-            mViewPager.setAdapter(pagerAdapter);
+            mViewPager.setAdapter(pagerAdapter)
 
-            SmartTabLayout viewPagerTab = view.findViewById(R.id.noviewpagertab);
-            viewPagerTab.setViewPager(mViewPager);
+            val viewPagerTab = view.findViewById<SmartTabLayout>(R.id.noviewpagertab)
+            viewPagerTab.setViewPager(mViewPager)
 
-            viewPagerTab.setOnPageChangeListener(new OnPageChangeListener() {
-                @Override
-                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+            viewPagerTab.setOnPageChangeListener(object : OnPageChangeListener {
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
                 }
 
-                @Override
-                public void onPageSelected(int position) {
+                override fun onPageSelected(position: Int) {
                     //if (position != 0) {
                     //pagerAdapter.getItem(position).onHiddenChanged(false);
                     //Fragment frag1 = (Fragment) pagerAdapter.instantiateItem(mViewPager, mViewPager.getCurrentItem());
 
-                    Fragment frag1 = pagerAdapter.getPage(position);
-                    if (frag1 != null)
-                        frag1.onHiddenChanged(false); // Refresh data
+                    val frag1 = pagerAdapter!!.getPage(position)
+                    if (frag1 != null) frag1.onHiddenChanged(false) // Refresh data
+
 
                     //}
                 }
 
-                @Override
-                public void onPageScrollStateChanged(int state) {
+                override fun onPageScrollStateChanged(state: Int) {
                 }
-            });
+            })
 
             // Locate the viewpager in activity_main.xml
             //ViewPager viewPager = view.findViewById(R.id.pager);
@@ -103,70 +92,79 @@ public class FontesPagerFragment extends Fragment {
             // Bind the tabs to the ViewPager
             //PagerSlidingTabStrip tabs = view.findViewById(R.id.tabs);
             //tabs.setViewPager(viewPager);
-
         }
 
         // Inflate the layout for this fragment
-        return view;
+        return view
     }
 
-//    public void onPageSelected(int position) {
-//        //.instantiateItem() from until .destroyItem() is called it will be able to get the Fragment of page.
-//        //Fragment page = pagerAdapter.getPage(position);
-//    }
-//
-//    public ViewPager getViewPager() {
-//        return (ViewPager) getView().findViewById(R.id.pager);
-//    }
+    private val viewPagerAdapter: FragmentPagerItemAdapter?
+        //    public void onPageSelected(int position) {
+        get() = ((requireView().findViewById<View?>(R.id.programrunner_pager)) as ViewPager).getAdapter() as FragmentPagerItemAdapter?
 
-    private FragmentPagerItemAdapter getViewPagerAdapter() {
-        return (FragmentPagerItemAdapter) ((ViewPager) (requireView().findViewById(R.id.programrunner_pager))).getAdapter();
-    }
-
-    @Override
-    public void onHiddenChanged(boolean hidden) {
+    override fun onHiddenChanged(hidden: Boolean) {
         if (!hidden) {
             // rafraichit le fragment courant
 
-            if (getViewPagerAdapter() != null) {
+            if (this.viewPagerAdapter != null) {
                 // Moyen de rafraichir tous les fragments. Attention, les View des fragments peuvent avoir ete detruit.
                 // Il faut donc que cela soit pris en compte dans le refresh des fragments.
-                Fragment frag1;
-                for (int i = 0; i < getViewPagerAdapter().getCount(); i++) {
-                    frag1 = getViewPagerAdapter().getPage(i);
-                    if (frag1 != null)
-                        frag1.onHiddenChanged(false); // Refresh data
+                var frag1: Fragment?
+                for (i in 0..<this.viewPagerAdapter!!.getCount()) {
+                    frag1 = this.viewPagerAdapter!!.getPage(i)
+                    if (frag1 != null) frag1.onHiddenChanged(false) // Refresh data
                 }
             }
         }
-    }
+    } //    public FontesFragment getFontesFragment() {
+    //        if (mpFontesFrag == null)
+    //            mpFontesFrag = (FontesFragment) getChildFragmentManager().findFragmentByTag(MainActivity.FONTES);
+    //        if (mpFontesFrag == null) mpFontesFrag = FontesFragment.newInstance(MainActivity.FONTES, 1);
+    //
+    //        //mpFontesFrag.onHiddenChanged(false);
+    //        return mpFontesFrag;
+    //    }
+    //
+    //    public FonteGraphFragment getGraphFragment() {
+    //        if (mpGraphFrag == null)
+    //            mpGraphFrag = (FonteGraphFragment) getChildFragmentManager().findFragmentByTag(MainActivity.GRAPHIC);
+    //        if (mpGraphFrag == null)
+    //            mpGraphFrag = FonteGraphFragment.newInstance(MainActivity.GRAPHIC, 2);
+    //
+    //        //mpGraphFrag.onHiddenChanged(false);
+    //        return mpGraphFrag;
+    //    }
+    //
+    //    public FonteHistoryFragment getHistoricFragment() {
+    //        if (mpHistoryFrag == null)
+    //            mpHistoryFrag = (FonteHistoryFragment) getChildFragmentManager().findFragmentByTag(MainActivity.HISTORY);
+    //        if (mpHistoryFrag == null)
+    //            mpHistoryFrag = FonteHistoryFragment.newInstance(-1, -1);
+    //
+    //        //mpHistoryFrag.onHiddenChanged(false);
+    //        return mpHistoryFrag;
+    //    }
 
-//    public FontesFragment getFontesFragment() {
-//        if (mpFontesFrag == null)
-//            mpFontesFrag = (FontesFragment) getChildFragmentManager().findFragmentByTag(MainActivity.FONTES);
-//        if (mpFontesFrag == null) mpFontesFrag = FontesFragment.newInstance(MainActivity.FONTES, 1);
-//
-//        //mpFontesFrag.onHiddenChanged(false);
-//        return mpFontesFrag;
-//    }
-//
-//    public FonteGraphFragment getGraphFragment() {
-//        if (mpGraphFrag == null)
-//            mpGraphFrag = (FonteGraphFragment) getChildFragmentManager().findFragmentByTag(MainActivity.GRAPHIC);
-//        if (mpGraphFrag == null)
-//            mpGraphFrag = FonteGraphFragment.newInstance(MainActivity.GRAPHIC, 2);
-//
-//        //mpGraphFrag.onHiddenChanged(false);
-//        return mpGraphFrag;
-//    }
-//
-//    public FonteHistoryFragment getHistoricFragment() {
-//        if (mpHistoryFrag == null)
-//            mpHistoryFrag = (FonteHistoryFragment) getChildFragmentManager().findFragmentByTag(MainActivity.HISTORY);
-//        if (mpHistoryFrag == null)
-//            mpHistoryFrag = FonteHistoryFragment.newInstance(-1, -1);
-//
-//        //mpHistoryFrag.onHiddenChanged(false);
-//        return mpHistoryFrag;
-//    }
+    companion object {
+        //    private String name;
+        //    private int id;
+        //    private FontesFragment mpFontesFrag = null;
+        //    private FonteHistoryFragment mpHistoryFrag = null;
+        //    private FonteGraphFragment mpGraphFrag = null;
+        /**
+         * Create a new instance of DetailsFragment, initialized to
+         * show the text at 'index'.
+         */
+        fun newInstance(name: String?, id: Int): FontesPagerFragment {
+            val f = FontesPagerFragment()
+
+            // Supply index input as an argument.
+            val args = Bundle()
+            args.putString("name", name)
+            args.putInt("id", id)
+            f.setArguments(args)
+
+            return f
+        }
+    }
 }
